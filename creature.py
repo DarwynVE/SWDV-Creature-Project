95016540af069223b8de92fdea9fc702ae268d4b
DarwynVE <90862287+DarwynVE@users.noreply.github.com>
2023-08-27 19:27:36 -0400

    Fixed

--- creature.py
+++ creature.py
@@ -2,7 +2,6 @@ import random
 
 class Creature:
     creatures = ["T-Rex", "Unicorn", "Raptor", "Spinosaurus"]
-    # health = random.randrange(100, 1000, 50)
     
     def __init__(self):
         print("Hey there! Welcome to the Creature Creation Project.")
@@ -22,8 +21,8 @@ class Creature:
             if secondOption not in self.creatures and secondOption != firstOption:
                 print("Please Select One Of The Creatures From The List That You Did Not Choose.")
             else:
-                self.creation = f"{firstOption[0:3]}{secondOption[4:]}"
-                print("Congratulations! You have created a", f"{firstOption[0:3]}{secondOption[4:]}.")
+                self.creation = f"{firstOption[0:3]}{secondOption[4:]}" # Stores the users creation
+                print("Congratulations! You have created a", f"{self.creation}.")
                 print("Here are some basic stats for your creation.")
                 print("Health Points: ", health)
                 print("Weight: ", weight, "Tons")
@@ -35,23 +34,11 @@ class Creature:
             print(f"Here is a picture of your {self.creation}.")
             if self.creation == "T-Rorn" or "Unix":
                 print("\ \n e-e         \n(\_/)\       \n `-'\ `--.___\n    '\( ,_.-'\n       '\ '\    \n")
-            if self.creation == "T-Ror" or "Rapx":
+            elif self.creation == "T-Ror" or "Rapx":
                 print("(\_/)\       \n `-'\ `--.___\n    '\( ,_.-'\n       '\ '\    \n")
                 
         
                 
 creature_creator = Creature()
 creature_creator.creatureCheck()
-creature_creator.ascii()
-    
-    # def creatureSelection(creatureOne, creatureTwo):
-    #     creatures = creatures
-    #     print(creatures)
-    #     def creatureOne():
-    #         for creature in creatures:
-    #             if firstOption == creature:
-    #                 return firstOption
-    #             else:
-    #                 return None
-    # def creatureTwo():
-    #     input("Choose your second creature: ")
+creature_creator.ascii()    
